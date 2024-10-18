37b2d9643cb3ce3ebe1676e8423d5c8901a676fd
rikelmi233 <144908907+rikelmi233@users.noreply.github.com>
2024-10-17 22:06:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é uma lista em Python?',
+    'Uma lista é uma coleção ordenada de elementos que pode conter diferentes tipos de dados.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Itália?',
+    'A capital da Itália é Roma.'
+)
+
+criaCartao(
+    'Programação',
+    ' Qual a finalidade de uma função em programação?',
+    'Uma função é um bloco de código que executa alguma tarefa.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz "obrigado" em inglês?',
+    'Obrigado" em inglês é "Thank you'
+)
+
+criaCartao(
+    'Cultura Geral',
+    'Qual é a maior floresta tropical do mundo?',
+    ' A Amazônia é a maior floresta tropical do mundo.'
+)
+
+criaCartao(
+    'Cartão de Estudo',
+    'Qual é o maior oceano do mundo?',
+    'O maior oceano do mundo é o Oceano Pacífico.'
+)
